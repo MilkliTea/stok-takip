956417fb8d57b9af34e23f34da0e8f134020ddbb
Alperen kr <76810431+MilkliTea@users.noreply.github.com>
2023-12-11 21:27:16 +0300

    Update Storage.php

--- src/Entity/Storage.php
+++ src/Entity/Storage.php
@@ -118,7 +118,7 @@ class Storage
     {
         return array_map(function ($storageProduct) {
             return [
-                'id' => $storageProduct->getProduct->getName(),
+                'id' => $storageProduct->getProduct()->getName(),
                 'name' => $storageProduct->getProduct()->getName(),
                 'quantity' => $storageProduct->getQuantity()
             ];
